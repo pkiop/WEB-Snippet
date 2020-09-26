2749c8a184b59bda9007ecd3a045d34ed9aee712
pkiop <vivezdv@gmail.com>
2020-09-26 15:29:53 +0900

    [init]

---
+++ src/test/templateLiteralForCreateElement.js
@@ -0,0 +1,23 @@
+const test = () => {
+  function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstElementChild; 
+  }
+  
+  const component = createElementFromHTML(`
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul
+  `)
+  
+  console.log(component);
+}
+
+(() => test())()
+
+export default test
